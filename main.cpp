12ff69ebb15477974fd28b53e53221d94ca159ca
Aiver7 <aiverson.palacios@udea.edu.co>
2025-11-10 18:44:15 -0500

    Se crearon las interacciones

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
+#include "network.h"
+#include "tools.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    network _network_;
+    int option = menu(0);
+    string aux;
+
+    if(option == 1){
+        cout << "Ingrese el nombre del archivo donde se encuentra la red\n -> ";
+        getline(cin, aux); system("cls");
+
+        _network_.load_network(aux);
+
+    }else{
+        cout << "red aleatoria aun no se hace XD" << endl;
+    }
+
+    while(true){
+        option = menu(1);
+
+        if(option == 1){
+            cout << "agregar enrutador" << endl;
+
+        }else if(option == 2){
+            cout << "eliminar enrutador" << endl;
+
+        }else if(option == 3){
+            cout << "Costo de envio" << endl;
+
+        }else if(option == 4){
+            cout << "Camino de envio" << endl;
+
+        }else{ break; };
+    }
+
     return 0;
 }
---
+++ tools.h
@@ -0,0 +1,11 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int menu(const int &type);
+
+#endif // TOOLS_H
